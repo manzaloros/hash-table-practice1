3771a19a78bc2d065fe108ee5efb01815266d309
Zach Mansell <zachmansell@gZachMansell.local>
2021-02-26 18:03:24 -0600

    Add hash table implementation and tests

--- HashTable.js
+++ HashTable.js
@@ -15,36 +15,111 @@ class LinkedList {
 
   addToTail(val) {
     const node = new Node(val);
-    if (this.head === this.tail) {
+    if (!this.head) {
+      this.tail = this.head = node;
+    } else if (this.head === this.tail) {
       this.head.next = node;
       this.tail = node;
     } else if (this.head && this.head !== this.tail) {
       this.tail.next = node;
       this.tail = node;
-    } else {
-      this.tail = this.head = node;
+    }
+  }
+
+  contains(val) {
+    if (!this.head) return false;
+    let node = this.head;
+
+    while (node) {
+      if (!Array.isArray(val)) {
+        if (node.val === val) return true;
+      } else if (Array.isArray(val) && val.length === 2) {
+        if (val[0] === node.val[0]) return true;
+      }
+      node = node.next;
+    }
+
+    return false;
+  }
+
+  remove(val) {
+    if (this.head === null) return;
+    if (Array.isArray(val)) {
+      const key = val[0];
+      if (this.head === this.tail) {
+        this.head = this.tail = null;
+      } else if (this.head.next === this.tail) {
+        if (this.head.val[0] === key) {
+          this.head = this.tail;
+        }
+      } else if (this.tail.val[0] === key) {
+        this.tail = this.head;
+        this.tail.next = null;
+      }
+    } else if (typeof val !== 'object' && val !== null) {
+      const node = this.head;
+      if (node.next === null) {
+        this.head = this.tail = null;
+        return;
+      }
+      if (node.val === val && node.next === null) {
+        this.head = this.tail = null;
+        return;
+      }
+
+      while (node) {
+        if (node.next.val === val) {
+          node.next = node.next.next;
+        }
+      }
     }
   }
 }
 
-function hashFunction(size) {
-
-}
-
 class HashTable {
   constructor(capacity) {
-    this.storage = [];
+    this.storage = Array.from(Array(10), () => new LinkedList());
     this.capacity = capacity;
     this.filled = 0;
   }
 
-  insert(key, value) {
+  getHashIndex(key) {
+    return key.toString().length % this.capacity;
+  }
 
+  insert(key, value) {
+    const tuple = [key, value];
+    const hashIndex = this.getHashIndex(key);
+    this.storage[hashIndex].addToTail(tuple);
   }
 
   retrieve(key) {
+    const hashIndex = this.getHashIndex(key);
+    const bucket = this.storage[hashIndex];
 
+    if (!bucket.head) return undefined;
+
+    if (bucket.tail === bucket.head) {
+      return bucket.tail.val[1];
+    }
+
+    if (bucket.tail.val[0] === key) {
+      return bucket.tail.val[1];
+    }
+
+    if (bucket.head.val[0] === key) {
+      return bucket.head.val[1];
+    }
+
+    return undefined;
+  }
+
+  remove(val) {
+    const hashIndex = this.getHashIndex(val);
+    const bucket = this.storage[hashIndex];
+
+    bucket.remove(val);
   }
 }
 
-module.exports = { HashTable, LinkedList, hashFunction };
+module.exports = { HashTable, LinkedList };
--- hashTable.test.js
+++ hashTable.test.js
@@ -1,15 +1,109 @@
-const { HashTable } = require('./HashTable');
+const { HashTable, LinkedList } = require('./HashTable');
+
+describe('LinkedList', () => {
+  let list = null;
+
+  beforeEach(() => {
+    list = new LinkedList();
+  });
+
+  test('it has an add to tail method', () => {
+    expect(typeof list.addToTail).toBe('function');
+  });
+
+  test('adding to the tail adds nodes to storage', () => {
+    list.addToTail(1);
+    expect(list.head.val).toBe(1);
+    expect(list.tail.val).toBe(1);
+
+    list.addToTail(2);
+    expect(list.head.val).toBe(1);
+    expect(list.tail.val).toBe(2);
+
+    list.addToTail(3);
+    expect(list.head.val).toBe(1);
+    expect(list.tail.val).toBe(3);
+  });
+
+  it('has a contains method that returns true if a value is in the list', () => {
+    list.addToTail(1);
+    expect(list.contains(1)).toBe(true);
+
+    const tuple = ['key', 'value'];
+    list.addToTail(tuple);
+    expect(list.contains(tuple)).toBe(true);
+  });
+
+  it('has a remove method that removes a node', () => {
+    list.addToTail('Harry Potter');
+    list.remove('Harry Potter');
+    expect(list.contains('Harry Potter')).toBe(false);
+  });
+});
 
 describe('hash table', () => {
   let hashTable = null;
+  const capacity = 10;
 
   beforeEach(() => {
-    hashTable = new HashTable(10);
+    hashTable = new HashTable(capacity);
+  });
+
+  it('is initialized with the correct capacity', () => {
+    expect(hashTable.capacity).toBe(capacity);
+  });
+
+  it('has storage that is initialized with capacity number of linked lists', () => {
+    expect(hashTable.storage.length).toBe(10);
+    expect(hashTable.storage[0] instanceof LinkedList).toBe(true);
   });
 
   it('has insert and retrieve methods', () => {
     expect(typeof hashTable.insert).toBe('function');
 
     expect(typeof hashTable.retrieve).toBe('function');
+  });
+
+  it('has storage that will contain an inserted value', () => {
+    hashTable.insert('dog', 'lenny');
+    let exists = false;
+    for (let i = 0; i < hashTable.storage.length; i += 1) {
+      const bucket = hashTable.storage[i];
+      if (bucket.head) {
+        if (bucket.head.val[0] === 'dog'
+      && bucket.head.val[1] === 'lenny') {
+          exists = true;
+          break;
+        }
+      }
+    }
+    expect(exists).toBe(true);
+  });
+
+  it('has a retrieve method that will return the inserted key', () => {
+    const [key, val] = ['George', 'Costanza'];
+    const [key2, val2] = ['Jerry', 'Seinfeld'];
+    hashTable.insert(key, val);
+    expect(hashTable.retrieve(key)).toBe(val);
+
+    hashTable.insert(key2, val2);
+    expect(hashTable.retrieve(key2)).toBe(val2);
+
+    hashTable.insert(key, val);
+    expect(hashTable.retrieve(key)).toBe(val);
+
+    hashTable.insert(key, val);
+    expect(hashTable.retrieve(key)).toBe(val);
+  });
+
+  it('returns undefined if trying to return a value that is not in storage', () => {
+    expect(hashTable.retrieve('Elaine')).toBeUndefined();
+  });
+
+  it('should not contain removed nodes', () => {
+    hashTable.insert(1);
+    hashTable.remove(1);
+    expect(hashTable.retrieve(1)).toBeUndefined();
+    expect(hashTable.remove('Dog Diggity')).toBeUndefined();
   });
 });
